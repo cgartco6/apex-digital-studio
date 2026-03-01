837e37eaa8921a4c6c761e3063d00f841400f870
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-03-01 17:28:32 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,34 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import { Toaster } from 'react-hot-toast';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+// Add this after other imports
+import GamificationOverlay from './components/gamification/GamificationOverlay';
+import { AddictionProvider } from './context/AddictionContext';
+
+// Wrap the entire app with AddictionProvider
+function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<Loading />} persistor={persistor}>
+        <AuthProvider>
+          <CartProvider>
+            <AIProvider>
+              <AddictionProvider>  {/* Add this */}
+                <Router>
+                  <div className="App">
+                    <GamificationOverlay />  {/* Add this */}
+                    <Toaster />
+                    {/* ... rest of the code ... */}
+                  </div>
+                </Router>
+              </AddictionProvider>  {/* Add this */}
+            </AIProvider>
+          </CartProvider>
+        </AuthProvider>
+      </PersistGate>
+    </Provider>
+  );
+}
 
 // Redux store
 import { store, persistor } from './redux/store';
